1933ccd9a53da91642f7d2db08ad570508e6b7bc
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2022-05-31 04:40:46 +0900

    simple as pie

--- calculator.js
+++ calculator.js
@@ -5,19 +5,13 @@ const calculator = {
     minus: function(firstNumber, secondNumber) {
         console.log(firstNumber - secondNumber);
     },
-    multiply: function(firstNumber, secondNumber) {
+    times: function(firstNumber, secondNumber) {
         console.log(firstNumber * secondNumber);
     },
     divide: function(firstNumber, secondNumber) {
         console.log(firstNumber / secondNumber);
     },
-    powerof: function(firstNumber, secondNumber) {  // 제곱
+    power: function(firstNumber, secondNumber) {  // 제곱
         console.log(firstNumber ** secondNumber);
     },
-}
-
-calculator.add(1, 2);
-calculator.minus(1, 2);
-calculator.multiply(1, 2);
-calculator.divide(1, 2);
-calculator.powerof(1, 2);
+};
